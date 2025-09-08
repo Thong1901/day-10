c5a7b532146993da67bab7ec74fd410945dac751
Thong1901 <thongthanhmai129@gmail.com>
2025-09-08 18:09:04 +0700

    test ci/cd

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-const app = express();
-const PORT = 3001;
-
-app.get('/', (req, res) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var app = (0, express_1.default)();
+var PORT = 3001;
+app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, function () {
+    console.log("Server is running on port ".concat(PORT));
 });
